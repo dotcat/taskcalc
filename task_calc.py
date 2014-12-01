4e59a04ca703804d36ccd72a5e1c082b2491c3ed
dotcat <dotcat.pink@gmail.com>
2014-12-01 04:46:01 +0100

    adding command line interface

--- task_calc.py
+++ task_calc.py
@@ -1,11 +1,11 @@
-import datetime
+#!/usr/bin/env python
+import sys
+command = int(sys.argv[1])
 
 w = 25
 sb = 5
 lb = 15
-t = [w, sb, w, sb, w, sb, w, lb,]
-
-
+t = [w, sb, w, sb, w, sb, w, lb, ]
 
 
 class TaskCalc():
@@ -13,7 +13,6 @@ class TaskCalc():
     def __init__(self, amount, template):
         self.template = template
         self.amount = amount
-
 
     def task_generator(self):
         i = r = len(self.template)
@@ -36,13 +35,11 @@ class TaskCalc():
 
     def pretty_duration(self):
         duration = self.calc_duration()
-        hours = duration//60
-        minutes = duration%60
+        hours = duration // 60
+        minutes = duration % 60
         return {'h': hours, 'm': minutes}
 
 
-
-
 if __name__ == '__main__':
-    pom = TaskCalc(7, t)
+    pom = TaskCalc(command, t)
     print(pom.pretty_duration())
